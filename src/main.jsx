24f8912401457d74e260ee516d8472c11064cf8b
quannguyen-1110 <quannhse184943@fpt.edu.vn>
2026-05-20 20:00:05 +0700

    update style

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './styles/global.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import App from './App.jsx';
-
+import './styles/student-theme.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
